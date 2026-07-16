40a661718c4863ab284184593ca4710b17475a0b
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-15 20:19:47 -0500

    Mejoras visuales

--- app.js
+++ app.js
@@ -7,8 +7,68 @@ const btnDecremento = document.getElementById("btnDecremento");
 btnIncremento.addEventListener("click", function(){
     contador++;
     puntaje.textContent = contador;
+
+    switch (contador){
+        case 10:
+            puntaje.classList.add("color-limite");
+            break;
+        case 20:
+            puntaje.classList.add("color-limite");
+            break;
+        case 30:
+            puntaje.classList.add("color-limite");
+            break;
+        case 40:
+            puntaje.classList.add("color-limite");
+            break;
+        case -10:
+            puntaje.classList.add("color-limite");
+            break;
+        case -20:
+            puntaje.classList.add("color-limite");
+            break;
+        case -30:
+            puntaje.classList.add("color-limite");
+            break;
+        case -40:
+            puntaje.classList.add("color-limite");
+            break;
+        default:
+            puntaje.classList.remove("color-limite");
+            break;
+    }
 })
 btnDecremento.addEventListener("click", function(){
     contador--;
     puntaje.textContent = contador;
+
+    switch (contador){
+        case 10:
+            puntaje.classList.add("color-limite");
+            break;
+        case 20:
+            puntaje.classList.add("color-limite");
+            break;
+        case 30:
+            puntaje.classList.add("color-limite");
+            break;
+        case 40:
+            puntaje.classList.add("color-limite");
+            break;
+        case -10:
+            puntaje.classList.add("color-limite");
+            break;
+        case -20:
+            puntaje.classList.add("color-limite");
+            break;
+        case -30:
+            puntaje.classList.add("color-limite");
+            break;
+        case -40:
+            puntaje.classList.add("color-limite");
+            break;
+        default:
+            puntaje.classList.remove("color-limite");
+            break;
+    }
 })
